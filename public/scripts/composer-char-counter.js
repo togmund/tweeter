c22f2aa33202553072668f2242fc264a431822ee
Ted Ogmundson <togmund@gmail.com>
2019-11-05 23:25:35 +0000

    able to console log the counter and current tweet length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,18 @@
 // Wait until the document is ready to run scripts
 $(document).ready(() => {
   
-  $(".new-tweet").on('click', function() {
-    const $textArea = $('textarea');
-    const tweetLength = $textArea.val().length;
-    console.log(tweetLength);
+  $(".new-tweet").on('change', function() {
+
+    // Get the text in the tweet area && the current tweet length
+    const $tweetArea = $('textarea');
+    const tweetLength = $tweetArea.val().length;
+
+    // Get the current counter
+    const $counter = $('.counter');
+    const counterValue = $counter.text();
+
+    console.log(`tweet length = ${tweetLength}`);
+    console.log(`counter value = ${counterValue}`);
   });
 
 });
