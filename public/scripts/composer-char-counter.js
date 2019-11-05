9804daadcdb7c9a648930cf368c9ba99fd29fbb9
Ted Ogmundson <togmund@gmail.com>
2019-11-05 23:34:07 +0000

    set a global maxTweetLength, set the counterValue to be the result of the difference between the max and the current character count in the new tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,20 @@
+// Set a maximum tweet length
+const maxTweetLength = 140
+
 // Wait until the document is ready to run scripts
 $(document).ready(() => {
   
-  $(".new-tweet").on('change', function() {
+  $(".new-tweet").on('keyup', function() {
 
     // Get the text in the tweet area && the current tweet length
     const $tweetArea = $('textarea');
-    const tweetLength = $tweetArea.val().length;
+    const newTweetLength = $tweetArea.val().length;
 
     // Get the current counter
     const $counter = $('.counter');
+    $counter.text(maxTweetLength - newTweetLength);
     const counterValue = $counter.text();
 
-    console.log(`tweet length = ${tweetLength}`);
-    console.log(`counter value = ${counterValue}`);
   });
 
 });
