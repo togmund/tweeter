9660131cc4a45842c9b3850cd11ed284e500fb2c
Ted Ogmundson <togmund@gmail.com>
2019-11-06 00:41:40 +0000

    re-factored .composer-char-counter to use 'this' best practices to improve performance and isolate side-effects to 'nearby' components

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,21 +3,22 @@ const maxTweetLength = 140
 
 // Wait until the document is ready to run scripts
 $(document).ready(() => {
-  
-  $(".new-tweet").on('keyup', function() {
+
+  $(".new-tweet form textarea").on('keyup', function () {
 
     // Get the text in the tweet area && the current tweet length
-    const $tweetArea = $('textarea');
-    const newTweetLength = $tweetArea.val().length;
+    const newTweetLength = $(this).val().length;
 
     // Get the current counter
-    const $counter = $('.counter');
-    $counter.text(maxTweetLength - newTweetLength);
-    const counterValue = $counter.text();
+    $(this).siblings('.counter').text(maxTweetLength - newTweetLength);
+    const counterValue = $(this).siblings('.counter').text();
 
-    // Set the color of the text to be red maybe?
-    $counter.toggleClass ('characterLimit', counterValue < 0 );
-    console.log(counterValue < 0);
+    // Set the color of the text to be red when over the limit;
+    $(this).siblings('.counter').toggleClass('characterLimit', counterValue < 0);
+
+    // Performance hit if it's global, shorter tree traversals with "this"
+    // Isolates to local pieces of the tree
+    // Web Components - Implemented in the browser - Super encapsulated
 
   });
 
