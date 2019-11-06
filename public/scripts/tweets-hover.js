e99a9d85b031f99a84820cdf76d1f639f926ffb8
Ted Ogmundson <togmund@gmail.com>
2019-11-06 04:26:58 +0000

    implemented on-hover behaviour. Last set of styling, then re-name html skeleton

--- public/scripts/tweets-hover.js
+++ public/scripts/tweets-hover.js
@@ -1,14 +1,9 @@
-// Set a maximum tweet length
-const maxTweetLength = 140
-
 // Wait until the document is ready to run scripts
 $(document).ready(() => {
 
-  $("article.tweet").on('click', function () {
-    // TODOs:
-    // Drop Shadow
-    // Hide @handle
-    // Reduce transparency of all elements
-  });
+  $( "article.tweet" ).hover(
+    function() { $( this ).addClass( "hover" ); },
+    function() { $( this ).removeClass( "hover" ); }
+  );
 
 });
