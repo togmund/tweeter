788058746ca0abeb1be3b7367cca92ea4be054f1
Ted Ogmundson <togmund@gmail.com>
2019-11-06 02:40:41 +0000

    crated tweets-hover.js file to handle hover-reaction. Added to-do lists to tweets-hover script and tweets.css stylesheet

---
+++ public/scripts/tweets-hover.js
@@ -0,0 +1,14 @@
+// Set a maximum tweet length
+const maxTweetLength = 140
+
+// Wait until the document is ready to run scripts
+$(document).ready(() => {
+
+  $("article.tweet").on('click', function () {
+    // TODOs:
+    // Drop Shadow
+    // Hide @handle
+    // Reduce transparency of all elements
+  });
+
+});
