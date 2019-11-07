c5cefbcf0a8d3ed8bfc9a5290b9d641e6bdc3f64
Ted Ogmundson <togmund@gmail.com>
2019-11-07 02:51:47 +0000

    rendering tweets inside the loadTweets callback function calling loadTweets on document.ready removed tweets-hover script in favor of :hover css style removed console logs, tested, confirmed functionality

--- public/scripts/tweets-hover.js
+++
@@ -1,9 +0,0 @@
-// Wait until the document is ready to run scripts
-$(document).ready(() => {
-
-  $( "article.tweet" ).hover(
-    function() { $( this ).addClass( "hover" ); },
-    function() { $( this ).removeClass( "hover" ); }
-  );
-
-});
